282e9f8be2135874ae29ce9d9a3580a067b20a01
Manuel Gómez <gomez.maman24@triana.salesianos.edu>
2024-01-29 10:32:08 +0100

    Ejemplo bucle for each

---
+++ EjemplosUD05/src/ejemploForMejorado/Principal.java
@@ -0,0 +1,21 @@
+package ejemploForMejorado;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		int[] numeros = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+		int suma = 0;
+
+		// Antes de los dos puntos se define una variable del tipo de lo que sea la
+		// variable. Después de los dos puntos aparece el nombre del array.
+
+		for (int valor : numeros) {
+			suma += valor;
+
+			System.out.println(valor);
+		}
+		System.out.println("La suma es: " + suma);
+	}
+
+}
