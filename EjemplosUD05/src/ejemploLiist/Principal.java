080c1b93094fb352627ea797cbd9f15dfdf979c3
Manuel Gómez <gomez.maman24@triana.salesianos.edu>
2024-02-02 10:17:29 +0100

    Locura

--- EjemplosUD05/src/ejemploLiist/Principal.java
+++ EjemplosUD05/src/ejemploLiist/Principal.java
@@ -27,6 +27,25 @@ public class Principal {
 
 		System.out.println(listado);
 
+		System.out.println("\n\nCambio\n\n");
+
+		List<Producto> listado2 = new ArrayList<Producto>();
+		Producto p = new Producto("Lechuga", 1.99);
+		Producto p2 = new Producto("Victor", 3);
+		Producto p3 = new Producto("Carlos", 1);
+
+		listado2.add(p);
+
+		listado2.add(p2);
+
+		System.out.println(listado2);
+
+		System.out.println(listado2.contains(p3));
+
+		listado2.remove(0);
+
+		System.out.println(listado2);
+
 	}
 
 }
