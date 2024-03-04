d7ef9fa58a96865a18b8a59f5c926733ae654fdb
Manuel <gomez.maman24@triana.salesianos.edu>
2024-03-04 16:01:10 +0100

    dsafas

--- EjemploProyectoSolo/src/ejemlo04/Principal.java
+++ EjemploProyectoSolo/src/ejemlo04/Principal.java
@@ -3,7 +3,14 @@ package ejemlo04;
 public class Principal {
 
 	public static void main(String[] args) {
-		int ListaNumeros[] = new int[5];
+		int listaNumeros[] = new int[5];
+
+		listaNumeros[0] = 1;
+		listaNumeros[1] = 2;
+		listaNumeros[2] = 3;
+		listaNumeros[3] = 4;
+		listaNumeros[4] = 5;
+
 	}
 
 }
